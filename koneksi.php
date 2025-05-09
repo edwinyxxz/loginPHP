4ece5be12fae5e8ea725d0ad59d6f181e0a8e7e8
edwinyxxz <edwinncahyo@gmail.com>
2025-05-09 10:17:54 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,6 +3,6 @@
 $koneksi = mysqli_connect("localhost", "root", "", "db_siswa");
 
 if (mysqli_connect_errno()){
-    echo "gagal kontol: " . mysqli_connect_error();
+    echo "koneksi gagal: " . mysqli_connect_error();
 }
 ?>
